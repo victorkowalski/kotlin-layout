ee2bbd131e129e7e7a1629f24faf0ab5dd59cdc4
vkowalski <vkowalski@lab-code.com>
2021-02-04 12:53:51 +0300

    initial commit

---
+++ app/src/main/java/com/victor/ko/layouts/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.victor.ko.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.test)
+        setContentView(R.layout.activity_search)
+    }
+}
